8a3610d0f754d2f2074babf83fea578f84b3201c
Frikk Balder <33499052+MindChirp@users.noreply.github.com>
2025-10-12 22:16:13 +0200

    Added title

--- app/non-authed/sign-in/index.tsx
+++ app/non-authed/sign-in/index.tsx
@@ -1,19 +1,49 @@
-import { View, Text } from 'react-native';
-import React from 'react';
-import { Image } from 'expo-image';
+import { cn } from '@/lib/cn';
 import { BlurView } from '@react-native-community/blur';
+import { Image } from 'expo-image';
+import React from 'react';
+import { Text, View } from 'react-native';
+import { useFonts, PixelifySans_400Regular } from '@expo-google-fonts/pixelify-sans';
 const AbakusLogo = require('@/assets/images/abakus-logo.png');
 
 const SignInPage = () => {
+  const [fontsLoaded] = useFonts({
+    PixelifySans_400Regular,
+  });
   return (
-    <View className="relative flex flex-col">
-      <BlurView className="absolute top-0 aspect-square w-full -translate-y-1/2 rounded-full bg-red-500/50" />
-      <View className="absolute top-1/2 h-96 min-w-full">
-        <Image source={AbakusLogo} className="" />
-      </View>
-      <Text>SignInPage</Text>
+    <View className="relative flex h-full flex-col">
+      <View className={cn(blobStyle, '-left-3/4 top-0 -translate-y-1/2')} />
+      <View className={cn(blobStyle, '-bottom-0 -right-3/4 translate-y-1/2')} />
+      <View className={cn(blobStyle, '-right-3/4 top-0  bg-[#0A613033]')} />
+      <View className={cn(blobStyle, '-left-3/4 bottom-0  bg-[#DBA7FD66]')} />
+      <BlurView
+        style={{
+          width: '100%',
+          position: 'relative',
+          zIndex: 20,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+        blurType="light"
+        blurAmount={30}
+        reducedTransparencyFallbackColor="white">
+        <View className="z-30 flex h-full w-full flex-col items-center justify-center">
+          <Text
+            style={{
+              fontFamily: 'PixelifySans_400Regular',
+            }}
+            className="text-primary text-5xl">
+            &gt; Velkommen
+          </Text>
+          {/* <Image source={AbakusLogo} className="h-fit w-full bg-red-500" /> */}
+        </View>
+      </BlurView>
     </View>
   );
 };
+
+const blobStyle = 'absolute z-10 aspect-square w-[150%] rounded-full bg-red-500/50';
 
 export default SignInPage;
