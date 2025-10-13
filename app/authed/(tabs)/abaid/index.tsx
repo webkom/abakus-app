6980ba89c44fb6791aaae12bfbc3a085c3d721a1
Frikk Balder <33499052+MindChirp@users.noreply.github.com>
2025-10-13 17:19:27 +0200

    Added fallback text

--- app/authed/(tabs)/abaid/index.tsx
+++ app/authed/(tabs)/abaid/index.tsx
@@ -2,7 +2,7 @@ import Cookie from '@/components/cookie';
 import { useUser } from '@/lib/hooks/useUser';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Image, View } from 'react-native';
+import { Image, Text, View } from 'react-native';
 import QRCodeStyled from 'react-native-qrcode-styled';
 import { SafeAreaView } from 'react-native-safe-area-context';
 const GreenGradient = require('@/assets/images/top-blur-green.png');
@@ -18,19 +18,24 @@ const AbaIDPage = () => {
           <View className="absolute">
             <Cookie />
           </View>
-          <QRCodeStyled
-            data={user?.username}
-            style={{
-              backgroundColor: 'transparent',
-              borderRadius: 16,
-              overflow: 'hidden',
-            }}
-            padding={20}
-            pieceBorderRadius={'50%'}
-            isPiecesGlued
-            color={'#904a4b'}
-            preserveAspectRatio="none"
-          />
+          {!user?.username && (
+            <Text className="w-full px-20 text-center uppercase">Ingen brukerdata</Text>
+          )}
+          {user?.username && (
+            <QRCodeStyled
+              data={user?.username}
+              style={{
+                backgroundColor: 'transparent',
+                borderRadius: 16,
+                overflow: 'hidden',
+              }}
+              padding={20}
+              pieceBorderRadius={'50%'}
+              isPiecesGlued
+              color={'#904a4b'}
+              preserveAspectRatio="none"
+            />
+          )}
         </View>
       </SafeAreaView>
     </View>
