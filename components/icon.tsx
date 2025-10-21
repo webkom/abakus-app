48cab48b45bedc708fed73e8f5bb17e0a84cac87
Frikk Balder <33499052+MindChirp@users.noreply.github.com>
2025-10-21 17:15:57 +0200

    Created icon component

---
+++ components/icon.tsx
@@ -0,0 +1,36 @@
+import { HomeIcon, icons } from 'lucide-react-native';
+import { cssInterop } from 'nativewind';
+import { ComponentProps, memo, useMemo } from 'react';
+
+type IconName = keyof typeof icons;
+
+/**
+ * The HomeIcon component is used since an arbitrary component has to be used to
+ * extract all props associated with a lucide icon component. It does not matter
+ * what component is used for this purpose. It could just as well had been
+ * PencilIcon or something else.
+ */
+type IconProps = { name: IconName; className?: string } & ComponentProps<typeof HomeIcon>;
+
+const Icon: React.FC<IconProps> = memo(({ name, className, ...props }) => {
+  const CustomIcon = useMemo(() => {
+    // eslint-disable-next-line import/namespace
+    const Icon = icons[name];
+    Icon.displayName = name;
+
+    return cssInterop(Icon, {
+      className: {
+        target: 'style',
+        nativeStyleToProp: {
+          color: true,
+          width: true,
+          height: true,
+        },
+      },
+    });
+  }, [name]);
+
+  return <CustomIcon className={className} {...props} />;
+});
+
+export default Icon;
