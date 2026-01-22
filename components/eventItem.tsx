3aae7e9c518815607f28774acf9d681e6de31719
Tony Ngo <tonyworep@gmail.com>
2026-01-22 22:08:27 +0100

    add event status and fix registration count

--- components/eventItem.tsx
+++ components/eventItem.tsx
@@ -1,52 +1,75 @@
 import React from 'react';
 import { Pressable, Text, View } from 'react-native';
-import { EventType } from '../lib/types/types';
+import { Event, EventStatusType, EventType } from '../lib/types/types';
 import { EventTypeConfig } from '../lib/types/eventColors';
 import { Link } from 'expo-router';
 import { format } from 'date-fns';
 import { nb } from 'date-fns/locale';
+import { Button } from './ui/button';
 
 interface EventItemProps {
   id: number;
-  title: string;
-  eventType: string;
-  startTime: string;
-  registrationCount: number;
-  totalCapacity: number;
+  event: Event;
 }
 
-export default function EventItem({
-  id,
-  title,
-  eventType,
-  startTime,
-  registrationCount,
-  totalCapacity,
-}: EventItemProps) {
-  const event = EventTypeConfig[eventType as EventType];
+export default function EventItem({ id, event }: EventItemProps) {
+  const eventColor = EventTypeConfig[event.eventType as EventType].color;
+
+  const getRegistrationStatus = () => {
+    if (
+      event.eventStatusType === EventStatusType.OPEN ||
+      event.eventStatusType === EventStatusType.INFINITE
+    ) {
+      return 'Åpent for alle';
+    }
+
+    if (event.isAdmitted) {
+      return 'Påmeldt';
+    }
+
+    if (event.userReg && event.eventStatusType === EventStatusType.NORMAL) {
+      return 'På venteliste';
+    }
+
+    if (event.activationTime) {
+      if (new Date(event.activationTime) > new Date()) {
+        return 'Snart åpent';
+      }
+      return 'Åpnet';
+    }
+
+    return 'Kan ikke melde på';
+  };
+
+  const getCapacity = () => {
+    if (!event.registrationCount) {
+      return `${event.totalCapacity} PLASSER`;
+    }
+
+    return `${event.registrationCount} / ${event.totalCapacity}`;
+  };
 
   return (
     <Link href={`/authed/(tabs)/events/${id}`} asChild>
-      <Pressable className="r-5 w-full rounded-xl active:bg-neutral-200">
+      <Button variant={'outline'} className="r-5 h-16 w-full rounded-xl active:bg-neutral-200">
         <View className="flex-row items-center justify-between">
-          <View className="rounded-lg px-1 py-9" style={{ backgroundColor: event.color }} />
+          <View className="rounded-lg px-1 py-8" style={{ backgroundColor: eventColor }} />
 
           <View className="mx-3 flex-1">
             <Text className="text-lg font-medium text-gray-900" numberOfLines={1}>
-              {title}
+              {event.title}
             </Text>
             <Text className="text-sm text-gray-600">
-              {format(new Date(startTime), 'eeeeee dd. MMM HH:mm', { locale: nb })}
+              {format(new Date(event.startTime), 'eeeeee dd. MMM HH:mm', { locale: nb })}
             </Text>
           </View>
 
           <View className="flex flex-col">
-            <Text className="rounded-xl bg-gray-200 px-2 py-1 font-semibold">
-              {registrationCount} / {totalCapacity}
-            </Text>
+            <Text>{getRegistrationStatus()}</Text>
+            <Text className={event.eventStatusType === EventStatusType.INFINITE ? "hidden" : "rounded-xl bg-gray-200 px-2 py-1 font-semibold"}>{getCapacity()}</Text>
           </View>
         </View>
-      </Pressable>
+      </Button>
     </Link>
   );
 }
