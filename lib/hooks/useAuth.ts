8722f75eee79d85edf83ac86c7e67f7b97158e56
Tony Ngo <tonyworep@gmail.com>
2026-08-07 12:09:32 +0200

    fix: fix try catch on unregister

--- lib/hooks/useAuth.ts
+++ lib/hooks/useAuth.ts
@@ -2,7 +2,6 @@ import { useMutation } from '@tanstack/react-query';
 import { login } from '../services/auth';
 import { userAtom } from '../atoms/user-atom';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { User } from '../types/user';
 import { useSetAtom } from 'jotai/react';
 import { api } from '../services/api';
 
@@ -24,7 +23,9 @@ export const useSignIn = () => {
   const signOut = async () => {
     try {
       await api.delete('/api/v1/device-expo/unregister/');
-    } catch { }
+    } catch (err) {
+      console.warn('Failed to unregister push token', err);
+    }
 
     await AsyncStorage.removeItem('user');
     await AsyncStorage.removeItem('session-token');
