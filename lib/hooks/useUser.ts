bd3f009dbc33499d2409d971ae128108b9943392
Tony Ngo <tonyworep@gmail.com>
2026-08-28 00:19:37 +0200

    remove: remove useless types and fix type for useUser

--- lib/hooks/useUser.ts
+++ lib/hooks/useUser.ts
@@ -1,12 +1,14 @@
 import { useAtomValue } from 'jotai/react';
 import { userAtom } from '../atoms/user-atom';
-import { GroupType } from '../types/user';
+import { components } from '../types/schema';
+
+type Group = components['schemas']['TypeEnum'];
 
 export const useUser = () => {
   const user = useAtomValue(userAtom);
 
   // Calcualte the user class
-  const grade = user?.abakusGroups?.find((group) => group.type === GroupType.Grade);
+  const grade = user?.abakusGroups?.find((group) => group.type === 'klasse');
 
   return { ...user, grade: grade?.name };
 };
