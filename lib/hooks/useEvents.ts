0812d365023d2ea92693dc7a2f2b666514e395bc
mindchirp <frikk44@gmail.com>
2026-09-01 13:44:12 +0200

    api debugging

--- lib/hooks/useEvents.ts
+++ lib/hooks/useEvents.ts
@@ -2,6 +2,7 @@ import { api } from '../services/api';
 
 export function useEvents() {
   const now = new Date().toISOString().split('T')[0];
+  console.log(now);
   return api.useQuery('get', '/api/v1/events/', {
     params: { query: { date_after: now } },
   });
